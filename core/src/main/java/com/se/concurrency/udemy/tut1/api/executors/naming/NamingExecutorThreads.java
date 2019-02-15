58c6a2a85d3aea55990846385d91559ce0a8075f
ndqvinh2109 <ndqvinh2109@gmail.com>
2019-02-15 23:27:12 +0800

    Refactor

--- core/src/main/java/com/se/concurrency/udemy/tut1/api/executors/naming/NamingExecutorThreads.java
+++ core/src/main/java/com/se/concurrency/udemy/tut1/api/executors/naming/NamingExecutorThreads.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class NamingExecutorThreads {
+
     public static void main(String[] args) {
 
         String currentThreadName = Thread.currentThread().getName();
